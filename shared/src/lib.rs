1f6d5f42a6a2854f1c1374d4754cfe31aac73f5f
Sven Schräer <sven.schraeer@porsche.de>
2025-12-02 11:20:05 +0100

    init base setup

---
+++ shared/src/lib.rs
@@ -0,0 +1,13 @@
+use std::{fs::File, io::{self, BufRead}, path::Path};
+
+
+
+pub fn read_lines<P>(filepath: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filepath)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+
